08f49f15455edd793c49384466318f6f261fa850
Elyrial <alexpersson03@proton.me>
2025-04-01 22:07:39 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+#[derive(Debug)]
+enum TaskStatus {
+    Pending,
+    Completed,
+}
+
+struct Task {
+    id: usize,
+    description: String,
+    status: TaskStatus,
+}
+
+impl Task {
+    fn new(id: usize, description: String) -> Self {
+        Self {
+            id,
+            description,
+            status: TaskStatus::Pending,
+        }
+    }
+
+    fn complete(&mut self) {
+        self.status = TaskStatus::Completed;
+    }
+}
+
+struct TodoList {
+    tasks: Vec<Task>,
+    next_id: usize,
+}
+
+impl TodoList {
+    fn new() -> Self {
+        Self {
+            tasks: Vec::new(),
+            next_id: 1,
+        }
+    }
+
+    fn add_task(&mut self, description: String) {
+        let task = Task::new(self.next_id, description);
+        self.tasks.push(task);
+        self.next_id += 1;
+    }
+
+    fn list_tasks(&self) {
+        for task in &self.tasks {
+            println!(
+                "ID: {} | {} | Status: {:?}",
+                task.id, task.description, task.status
+            );
+        }
+    }
+
+    fn complete_task(&mut self, task_id: usize)  {
+        if let Some(task) = self.tasks.iter_mut().find(|t| t.id == task_id) {
+            task.complete();
+            println!("Task {} has been marked as complete", task_id);
+        } else {
+            println!("Task could not be found");
+        }
+    }
+}
+
+fn main() {
+    let mut todo_list = TodoList::new();
+
+    todo_list.add_task("Learn Rust".to_string());
+    todo_list.add_task("Build a todo list".to_string());
+
+    todo_list.list_tasks();
+
+    todo_list.complete_task(1);
+    todo_list.list_tasks();
+
+}
+
+
+
+
